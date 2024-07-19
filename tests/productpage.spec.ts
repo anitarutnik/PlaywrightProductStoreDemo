9af0f70e98b330ae61aa4ab68b691edfbc3cd497
Anita Rutnik <anita.rutnik.os@gmail.com>
2024-07-19 12:40:34 +0200

    .

--- tests/productpage.spec.ts
+++ tests/productpage.spec.ts
@@ -8,7 +8,7 @@ test('add to cart success', async ({ page, productPage }) => {
 
         //Check the dialog message
         await expect(dialog.message()).toBe('Product added');
-        
+
         //Click the Ok button
         await dialog.accept();
 
@@ -25,13 +25,23 @@ test('add to cart success', async ({ page, productPage }) => {
 });
 
 test('check if phone is in the cart', async ({ productPage }) => {
+    // Navigate to the product page
     await productPage.goto();
 
+    // Click on the product link to go to the product's detail page
     await productPage.productLink.click();
+
+    // Wait for the 'Add to Cart' button to be visible and then click it
+    await productPage.addBtn.waitFor({ state: 'visible' });
     await productPage.addBtn.click();
+
+    // Wait for the cart link to be visible and then click it
+    await productPage.cartLink.waitFor({ state: 'visible' });
     await productPage.cartLink.click();
 
-    //Expects product to be added to cart
-    await expect(productPage.addedProduct).toBeVisible();
+    // Wait for the added product to be visible in the cart
+    await productPage.addedProduct.waitFor({ state: 'visible' });
 
+    // Expect the product to be visible in the cart
+    await expect(productPage.addedProduct).toBeVisible();
 });
