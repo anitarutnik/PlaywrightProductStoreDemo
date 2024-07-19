808bcdd71484753b3da01f069f6e5de2aab63b5f
Anita Rutnik <anita.rutnik.os@gmail.com>
2024-07-19 14:17:15 +0200

    Fix

--- tests/productpage.spec.ts
+++ tests/productpage.spec.ts
@@ -25,30 +25,35 @@ test('add to cart success', async ({ page, productPage }) => {
 });
 
 test('check if phone is in the cart', async ({ page, productPage }) => {
-    // Navigate to the product page
     await productPage.goto();
-    // Click on the product link to go to the product's detail page
-    await productPage.productLink.click();
-    
-    page.on('dialog', async dialog => {
-        expect(dialog.message()).toBe('Product added');
-        await dialog.accept();
 
-        // Wait for the cart link to be visible and then click it
-        await productPage.cartLink.waitFor({ state: 'visible' });
-        await productPage.cartLink.click();
-    
-        // Wait for the added product to be visible in the cart
-        await productPage.addedProduct.waitFor({ state: 'visible' });
-    
-        // Expect the product to be visible in the cart
-        await expect(productPage.addedProduct).toBeVisible();
+    //Wait for the alert to appear and verify its content
+    page.on('dialog', async dialog => {
+
+        //Check the dialog message
+        await expect(dialog.message()).toBe('Product added');
+
+        //Click the Ok button
+        await dialog.accept();
 
     });
 
-    // Wait for the 'Add to Cart' button to be visible and then click it
-    await productPage.addBtn.waitFor({ state: 'attached' });
-    await productPage.addBtn.click({ delay: 500 });
+    //Click the product link.
+    await productPage.productLink.click();
+
+    //Click Add to cart
+    await productPage.addBtn.click();
+
+    
+    // Wait for the cart link to be visible and then click it
+    await productPage.cartLink.waitFor({ state: 'visible' });
+    await productPage.cartLink.click();
+    
+    // Wait for the added product to be visible in the cart
+    await productPage.addedProduct.waitFor({ state: 'visible' });
+    
+    // Expect the product to be visible in the cart
+    await expect(productPage.addedProduct).toBeVisible();
 });
 
 test('product visual comparisons test', async ({ page, productPage }) => {
